15491ac30fe2234540adec13ad5415687cf14f4f
BOMBONS <bombons@BOMNONSs-iMac-Pro.local>
2019-06-16 10:13:07 +0200

    update, 16-06-2019 10:13:07

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# x99
+# X99
+
+Some automated scripts to setup a x99 hack. All scripts and procedure are based on the works of the legends RehabMan ans KGP.
+
+## create CLOVER installer
+
+```bash
+./download.sh
+```
+
+## installation
+
+```bash
+./download.sh
+./install_downloads.sh
+```
+
+### extra
+
+By default FakeSMC sensors are not installed, if you want it run:
+
+```bash
+./install_sensors.sh
+```
+
+If you want only install tools:
+```bash
+./install_tools.sh
+```
+
+# commands
+
+view non apple kexts loaded
+
+```bash
+kextstat | grep -v com.apple
+```
--- download.sh
+++ download.sh
@@ -15,8 +15,13 @@ if [[ ! -d ./downloads ]]; then mkdir ./downloads; fi && rm -Rf ./downloads/* &&
 mkdir ./kexts && cd ./kexts || exit 1
 download_rehabman os-x-fakesmc-kozlek RehabMan-FakeSMC
 download_rehabman os-x-intel-network RehabMan-IntelMausiEthernet
+download_rehabman os-x-eapd-codec-commander RehabMan-CodecCommander
 download_acidanthera Lilu acidanthera-Lilu
+download_acidanthera AppleALC acidanthera-AppleALC
+download_acidanthera WhateverGreen acidanthera-WhateverGreen
 download_bombons TSCAdjustReset TSCAdjustReset interferenc-TSCAdjustReset
+download_bitbucket_bombons agpminjector AGPMInjector
+download_bitbucket_bombons unsolid UnSolid
 cd ..
 
 # download tools
